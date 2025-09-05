7e678403ee26908c5773eb45abca6362c308ff06
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-09-05 06:48:21 +0100

    Added the neccessary files for all the python related tasks

---
+++ python-generators-0x00/seed.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+seed = __import__('seed')
+
+connection = seed.connect_db()
+if connection:
+    seed.create_database(connection)
+    connection.close()
+    print(f"connection successful")
+
+    connection = seed.connect_to_prodev()
+
+    if connection:
+        seed.create_table(connection)
+        seed.insert_table(connection, 'user_data.csv')
+        cursor = connection.cursor()
