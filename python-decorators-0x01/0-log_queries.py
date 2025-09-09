2aaa5dc5a231896e90a531d7c91b793a97f7d26a
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-09-09 19:15:51 +0100

    log_queries corrected

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,29 +1,6 @@
 import sqlite3
 import functools
-
-
-"""
-    import sqlite3
-    cx = sqlite3.connect("test.db")  # test.db will be created or opened
-    The special path name ":memory:" can be provided to connect to a transient in-memory database:
-
-    cx = sqlite3.connect(":memory:")  # connect to a database in RAM
-    Once a connection has been established, create a Cursor object and call its execute() method to perform SQL queries:
-
-    cu = cx.cursor()
-
-      # create a table
-    cu.execute("create table lang(name, first_appeared)")
-
-    # insert values into a table
-    cu.execute("insert into lang values (?, ?)", ("C", 1972))
-
-    # execute a query and iterate over the result
-    for row in cu.execute("select * from lang"):
-        print(row)
-
-    cx.close()
-"""
+from datetime import datetime
 
 # decorator to lof SQL queries
 
@@ -31,9 +8,14 @@ import functools
 
 
 def log_queries(func):
-    def wrapper(query):
-        query = ("SET GLOBAL general_log = 'on';")
-        func(query)
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        query = kwargs.get(
+            "query") if "query" in kwargs else args[0] if args else None
+        if query:
+            print(f"[{datetime.now()}] Executing query: {query}")
+        result = func(*args, **kwargs)
+        return result
     return wrapper
 
 
