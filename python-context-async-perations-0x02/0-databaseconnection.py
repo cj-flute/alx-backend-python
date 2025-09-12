34434745205996182ebaa5737f7adf94e78428b6
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-09-12 17:52:47 +0100

    Custom class based context manager for database connection

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+#  custom class based context manager for Database connection
+
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+
+    def __enter__(self):
+        import sqlite3
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type:
+            print(f"An error occurred: {exc_value}")
+            self.conn.rollback()
+
+        if self.conn:
+            self.conn.close()
+
+
+db_conn = sqlite3.connect('users.db')
+
+with DatabaseConnection(db_conn) as conn:
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    print(users)
