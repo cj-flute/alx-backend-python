f908f801819e1234328535b201de688670cfdf8b
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-09-13 00:41:35 +0100

    async

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -3,24 +3,26 @@ import asyncio
 import aiosqlite
 
 
-async def async_fetch_users(connection):
-    async with connection.execute("SELECT * FROM users") as cursor:
-        return await cursor.fetchall()
+async def async_fetch_users():
+    async with aiosqlite.connect("users.db") as conn:
+        async with conn.execute("SELECT * FROM users") as cursor:
+            return await cursor.fetchall()
 
 
-async def async_fetch_older_users(connection):
-    async with connection.execute("SELECT * FROM users WHERE age > 40") as cursor:
-        return await cursor.fetchall()
+async def async_fetch_older_users():
+    async with aiosqlite.connect("users.db") as conn:
+        async with conn.execute("SELECT * FROM users WHERE age > 40") as cursor:
+            return await cursor.fetchall()
 
 
 async def fetch_concurrently():
-    async with aiosqlite.connect('users.db') as conn:
-        all_users, older_users = await asyncio.gather(
-            async_fetch_users(conn),
-            async_fetch_older_users(conn)
-        )
-        print("All Users:", all_users)
-        print("Older Users:", older_users)
+    results = await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users()
+    )
+    all_users, older_users = results
+    print("All Users:", all_users)
+    print("Older Users:", older_users)
 
 
 asyncio.run(fetch_concurrently())
