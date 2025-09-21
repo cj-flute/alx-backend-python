1b25ff59475c0ba555fe6288c5c63be3173cf15f
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-09-21 23:49:09 +0100

    django REST Framework and setting it up in the settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -76,6 +76,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',  # optional
+    ],
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
