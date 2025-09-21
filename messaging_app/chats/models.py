839e83ef32d03fc6663b6a4494eb4b96f7f16f3b
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-09-22 00:56:40 +0100

    Define data Models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+
+class User(AbstractUser):
+    user_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    ROLE_CHOICES = [
+        ('guest', 'Guest'),
+        ('host', 'Host'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(
+        max_length=10, choices=ROLE_CHOICES, default='guest')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # email unique
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.email})"
+
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(User, related_name="conversations")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.conversation_id}"
+
+
+class Message(models.Model):
+    message_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    sender = models.ForeignKey(
+        User, related_name="sent_messages", on_delete=models.CASCADE)
+    conversation = models.ForeignKey(
+        Conversation, related_name="messages", on_delete=models.CASCADE)
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender} in {self.conversation} at {self.sent_at}"
