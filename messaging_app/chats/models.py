c458020145e497a0f27ac2ce7752cdb3f998d9f7
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-09-22 01:02:03 +0100

    Define data Models corrected

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -6,6 +6,10 @@ import uuid
 class User(AbstractUser):
     user_id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=150, null=False)
+    last_name = models.CharField(max_length=150, null=False)
+    email = models.EmailField(unique=True, null=False)
+    password = models.CharField(max_length=256, null=False)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     ROLE_CHOICES = [
         ('guest', 'Guest'),
