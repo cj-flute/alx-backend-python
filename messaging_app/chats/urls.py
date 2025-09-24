70712e5698e43239e1bc490b6144547c74c73103
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-09-24 22:32:05 +0100

    url fix

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from .views import ConversationViewSet, MessageViewSet
 
 # Create a router and register our viewsets with it
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
+
+# Nested router for messages under conversations
+conversations_router = routers.NestedDefaultRouter(
+    router, r'conversations', lookup='conversation')
+conversations_router.register(
+    r'messages', MessageViewSet, basename='conversation-messages')
 
 # The API URLs are now determined automatically by the router
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(conversations_router.urls)),
 ]
