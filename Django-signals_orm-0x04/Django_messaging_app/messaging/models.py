0bccbcdabb054357fa0bb2020636916e9b947206
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-10-05 20:26:07 +0100

    Implement Signals for User Notifications

---
+++ Django-signals_orm-0x04/Django_messaging_app/messaging/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Content(models.Model):
+    content_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(User, related_name="contents")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.content_id}"
+
+
+class Message(models.Model):
+    message_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    sender = models.ForeignKey(
+        User, related_name="sent_messages", on_delete=models.CASCADE)
+    receiver = models.ForeignKey(
+        User, related_name="received_messages", on_delete=models.CASCADE)
+    conversation = models.ForeignKey(
+        Content, related_name="messages", on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender} to {self.receiver}"
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="notifications")
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification for {self.user.username}"
