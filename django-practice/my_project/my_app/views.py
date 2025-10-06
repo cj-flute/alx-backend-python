00c560456353c96ad7c1fde9c409cbc1bd5070e6
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-10-06 18:46:29 +0100

    Folder rearrangements

--- django-practice/my_project/my_app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Book
-from rest_framework import generics
-from .serializers import BookSerializer
-
-
-class BookListCreateView(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
