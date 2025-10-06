db1e9c3af44adfcaf2d8899b60ac5364d1f78921
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-10-06 20:10:24 +0100

    Use Signals for Deleting User-Related Data

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
-from .models import Message, Notification, MessageHistory
+from .models import User, Message, Notification, MessageHistory
 
 
 @receiver(post_save, sender=Message)
@@ -39,3 +39,28 @@ def save_old_message_content(sender, instance, **kwargs):
         # Mark message as edited
         instance.edited = True
         print(f"Message '{instance.pk}' was edited. Old version saved.")
+
+
+@receiver(post_delete, sender=User)
+def cleanup_related_data(sender, instance, **kwargs):
+    """
+    When a User is deleted, clean up all related data manually if needed.
+    """
+    user = instance
+
+    # Delete sent and received messages
+    sent_msgs = Message.objects.filter(sender=user)
+    received_msgs = Message.objectsfilter(receiver=user)
+
+    for msg in sent_msgs.union(received_msgs):
+        # Delete related histories
+        MessageHistory.objects.filter(Message=msg).delete()
+        # Delete related notifications
+        Notification.objects.filter(message=msg).delete()
+        msg.delete()
+
+    # Delete any remaining notifications directly linked to the user
+    Notification.objects.filter(user.user).delete()
+
+    print(
+        f"All message, notifications, and histories for '{user.username}' have been deleted")
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+# messaging/views.py
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+
+@login_required
+def delete_user(request):
+    user = request.user
+    username = user.username
+
+    # Delete the user (triggers post_delete signal)
+    user.delete()
+
+    messages.success(
+        request, f"Account '{username}' has been deleted successfully.")
+    return redirect('home')  # replace 'home' with your homepage route name
